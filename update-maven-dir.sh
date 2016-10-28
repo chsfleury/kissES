740c9aa5328e440756bb27c49ee25461c296557a
Charles Fleury <noxx90@gmail.com>
2016-10-28 19:19:21 +0200

    create maven script

---
+++ update-maven-dir.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+VERSION=5.0.0.0-SNAPSHOT
+
+mvn clean install -DskipTests
+
+mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-core -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-core/target/kisses-core-$VERSION.jar -DlocalRepositoryPath=maven
+mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-annotations -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-annotations/target/kisses-annotations-$VERSION.jar -DlocalRepositoryPath=maven/
