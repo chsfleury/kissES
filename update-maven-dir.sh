890b8b0c33fcead4941f4591cb5acda0f5f62f68
Charles Fleury <noxx90@gmail.com>
2016-11-02 10:50:16 +0100

    update dist: add kisses-spring4

--- update-maven-dir.sh
+++ update-maven-dir.sh
@@ -6,3 +6,4 @@ mvn clean install -DskipTests
 
 mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-core -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-core/target/kisses-core-$VERSION.jar -DlocalRepositoryPath=maven
 mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-annotations -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-annotations/target/kisses-annotations-$VERSION.jar -DlocalRepositoryPath=maven/
+mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-spring4 -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-spring4/target/kisses-spring4-$VERSION.jar -DlocalRepositoryPath=maven/
