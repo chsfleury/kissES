a69c6c29020259c20850e060675d0ba76b3ec851
Charles Fleury <noxx90@gmail.com>
2016-11-02 11:45:40 +0100

    fix poms

--- update-maven-dir.sh
+++ update-maven-dir.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-VERSION=5.0.0.0-SNAPSHOT
-
 mvn clean install -DskipTests
 
-mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-core -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-core/target/kisses-core-$VERSION.jar -DlocalRepositoryPath=maven
-mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-annotations -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-annotations/target/kisses-annotations-$VERSION.jar -DlocalRepositoryPath=maven/
-mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-spring4 -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-spring4/target/kisses-spring4-$VERSION.jar -DlocalRepositoryPath=maven/
+cp -ru $HOME/.m2/repository/org/kisses/ maven/org/
+
+#mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-core -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-core/target/kisses-core-$VERSION.jar -DlocalRepositoryPath=maven
+#mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-annotations -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-annotations/target/kisses-annotations-$VERSION.jar -DlocalRepositoryPath=maven/
+#mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-spring4 -Dversion=$VERSION -Dpackaging=jar -Dfile=kisses-spring4/target/kisses-spring4-$VERSION.jar -DlocalRepositoryPath=maven/
+#
+#mvn install:install-file -DgroupId=org.kisses -DartifactId=kisses-spring4-starter -Dversion=$VERSION -Dpackaging=pom -Dfile=kisses-spring4/target/kisses-spring4-starter-$VERSION.pom -DlocalRepositoryPath=maven/
